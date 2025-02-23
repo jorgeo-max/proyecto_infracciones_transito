36e78ad15915eb005c3ffc692695c4e01f7e16f4
jorgeo-max <118086016+jorgeo-max@users.noreply.github.com>
2025-02-23 12:59:08 -0500

    Add files via upload
    Se mejoro el bot, ahora identifica el número del estrato ingresado en palabras y se agregó un diccionario para definir los números del 1 al 20.

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import wordnet
 from fastapi.templating import Jinja2Templates
-from fastapi.requests import Request
 
 # Configuración de NLTK
 nltk.data.path.append("c:/Users/jorgeestrada/Library/Python/3.9/lib/python/site-packages/nltk")
@@ -19,11 +18,6 @@ app = FastAPI(title="Infracciones de Tránsito", version="1.0.0")
 
 # Definimos la ruta donde esta el archivo index.html para darle formato a la página
 templates = Jinja2Templates(directory="/Users/jorgeestrada/BOOTCAMP_AI/proyecto_final/templates/index.html")
-
-"""Extrae el primer número de estrato socioeconómico encontrado en la consulta del usuario."""
-def extract_estrato(texto):
-    match = re.search(r'\b\d+\b', texto) # Busca cuálquier número entero en el texto ingresado por el usuario
-    return match.group() if match else "Estrato socioeconómico no encontrado"
 
 # Carga el dataset "datos_infracciones.csv" en la aplicación
 def load_data(file_path: str):
@@ -44,10 +38,27 @@ def load_data(file_path: str):
 # Definimos la ruta donde tenemos el dataset "datos_infracciones.csv"
 DATA_FILE = "/Users/jorgeestrada/BOOTCAMP_AI/proyecto_final/datos_infracciones.csv"
 try:
-    datos_list = load_data(DATA_FILE)
+        datos_list = load_data(DATA_FILE)
 except RuntimeError as e:
-    datos_list = []
-    print(e)
+        datos_list = []
+        print(e)
+
+# Definimos el Diccionario para convertir un número en palabras a números
+palabras_a_numeros = {
+        "uno": "1", "dos": "2", "tres": "3", "cuatro": "4", "cinco": "5", "seis": "6", "siete": "7", "ocho": "8", "nueve": "9", "diez": "10", "once": "11", "doce": "12", "trece": "13", "catorce": "14", "quince": "15", "dieciseis": "16", "diecisiete": "17", "dieciocho": "18", "diecinueve": "19", "veinte": "20"
+    }
+               
+"""Extrae el primer número de estrato socioeconómico encontrado en la consulta del usuario."""
+def extract_estrato(texto):
+    match = re.search(r'\b\d+\b', texto) # Busca cuálquier número entero en el texto ingresado por el usuario
+    if match: 
+        return match.group()
+    
+    palabras = texto.lower().split()
+    for palabra in palabras:
+        if palabra in palabras_a_numeros:
+            return palabras_a_numeros[palabra]
+    return None
 
 # Función para obtener sinónimos.
 def get_synonyms(word: str):
@@ -63,7 +74,7 @@ def get_datos(id: str):
     data = next((item for item in datos_list if item['id'] == id), None)
     if data:
         return data
-    raise HTTPException(status_code=404, detail="Registro no existe en la base de datos")
+    raise HTTPException(status_code=404, detail="¡¡¡Registro no existe en la base de datos!!!")
 
 @app.get('/chatbot', tags=['Chatbot para consulta de Infracciones según el estrato socioeconómico del infractor'])
 def chatbot(query: str = Query(..., description="Consulta relacionada con el estrato socioeconómico del infractor:")):
@@ -71,12 +82,15 @@ def chatbot(query: str = Query(..., description="Consulta relacionada con el est
  
  # Obtener sinónimos de las palabras en la consulta   
     synonyms = {word for q in query_words for word in get_synonyms(q)} | set(query_words)
+    
+ # Extraer el número o texto del estrato socioeconómico ingresado por el usuario o usar el primer valor encontrado o dejar vacio
+    ssb_value = extract_estrato(query)
+    if ssb_value is None:
+        return JSONResponse(content={"respuesta": f"¡¡¡Estrato socioeconómico no válido!!!, por favor ingrese un valor entre 1 y 6.", "Multas": []})
+    # Filtrar los resultados en datos_list comparando los valores   
+    results = [item for item in datos_list if str(item['ssb']).strip() == ssb_value]
+    
+    response_message = f"Aquí tienes algunas multas relacionadas con el estrato socioeconómico: {ssb_value}" if results else f"Estrato ingresado: {ssb_value}. ¡¡¡Estrato socioeconómico no válido!!!, por favor ingrese un valor entre 1 y 6."
  
- # Filtrar los resultados según los sinónimos   
-    results = [item for item in datos_list if any(s in item['ssb'].lower() for s in synonyms)]
-    
- # Extraer el número del estrato socioeconómico ingresado por el usuario o usar el primer valor encontrado o dejar vacio
-    ssb_value = results[0]['ssb'] if results else extract_estrato(query)
-    response_message = f"Aquí tienes algunas multas relacionadas con el estrato socioeconómico: {ssb_value}" if results else f"Estrato ingresado: {ssb_value}. Estrato socioeconómico no válido, por favor ingrese un valor entre 1 y 6."
-    
+ # Devolver la respuesta    
     return JSONResponse(content={"respuesta": response_message, "Multas": results})
